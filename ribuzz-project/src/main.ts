cd3a692f615726af6ed6a80029f9b713f0d669af
Camilo <pedrazacamilo630@gmail.com>
2024-08-21 21:18:02 -0500

    Creacion Servicios de Usuario

--- ribuzz-project/src/main.ts
+++ ribuzz-project/src/main.ts
@@ -1,7 +1,7 @@
 /* eslint-disable prettier/prettier */
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import "reflect-metadata"
+import 'reflect-metadata';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
